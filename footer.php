40da9c8438f1b4a509f93375992f13b952c48dc4
jo <pangilinanjorlyn@gmail.com>
2025-11-30 01:00:57 +0800

    updates and on-going updates

---
+++ footer.php
@@ -0,0 +1,3 @@
+<footer>
+    &copy; 2025 - Pangilinan, Jorlyn M.
+</footer>
---
+++ header.php
@@ -0,0 +1,7 @@
+<h1>The Flower Store</h1>
+<nav>
+    <a href="index.php">Home</a>
+    <a href="favorites.php">Favorites</a>
+    <a href="cart.php">Shopping Cart</a>
+    <a href="demo.php">Demo</a>
+</nav>
